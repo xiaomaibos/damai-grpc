cbc5149bdc3bd9e39a32484ab0627972e382ba20
zzzz76 <15079084006@163.com>
2021-05-21 01:50:48 +0800

    Add user-client

---
+++ damai-userclient/src/main/java/cn/ecnu/damai/controller/Response/ResultMap.java
@@ -0,0 +1,39 @@
+package cn.ecnu.damai.controller.Response;
+
+import com.alibaba.fastjson.JSON;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class ResultMap implements Serializable {
+    private boolean success;
+    private String message;
+    private Integer code;
+    private Object data;
+
+    public ResultMap(ResultType resultType) {
+        this.success = resultType.success;
+        this.code = resultType.code;
+        this.message = resultType.message;
+    }
+
+    public ResultMap(ResultType resultType, Object data) {
+        this.success = resultType.success;
+        this.code = resultType.code;
+        this.message = resultType.message;
+        this.data = data;
+    }
+
+    public ResultMap(ResultType resultType, String message) {
+        this.success = resultType.success;
+        this.code = resultType.code;
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return JSON.toJSONString(this);
+    }
+
+}
